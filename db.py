d61a3b4572aa98ddcb0f321a66736fd38ac551b8
raceee <race.peterson@gmail.com>
2023-04-04 23:23:35 -0600

    db changes

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import yaml
 with open('config.yaml', 'r') as file:
     mongo_creds = yaml.safe_load(file)
 
-myclient = pymongo.MongoClient()
+myclient = pymongo.MongoClient("mongodb+srv://{}:{}@bitcoinpred.mknwdgp.mongodb.net/?retryWrites=true&w=majority".format(mongo_creds["mongo_username"],mongo_creds["mongo_password"]))
 
 mydb = myclient["bitcoin_data"]
 print(mydb)
