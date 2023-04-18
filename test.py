1915a4c95f718cf7d8f288df268cbee958f3e236
raceee <race.peterson@gmail.com>
2023-04-17 22:08:34 -0600

    net testing

--- test.py
+++ test.py
@@ -21,42 +21,46 @@ test_dataloader = DataLoader(test_data, batch_size=16, shuffle=True)
 model = SequenceModel()
 
 # optimizer
-optimizer = torch.optim.Adam(model.parameters(),lr=0.1)
+optimizer = torch.optim.Adam(model.parameters(),lr=1)
 scheduler = ExponentialLR(optimizer, gamma=0.9)
 
-best_epoch = (0,100000000)
+best_epoch = {"epoch_num":0, "loss":1000000000}
 epoch_count = 0
 noprog = 0
 while True:
     print("Epoch {} started".format(epoch_count))
     running_loss = 0
-    if noprog >= 3:
-        break
     for i, data in enumerate(train_dataloader):
         # print(data)
         input, label = data
         optimizer.zero_grad()
         pred = model(input)
-        # print(torch.squeeze(pred), label)
-        # loss = F.mse_loss(torch.squeeze(pred), label)
         loss = torch.sqrt((torch.squeeze(pred) - label)**2).sum()
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        if i == len(train_dataloader) - 1:
-            avg_loss = running_loss / (i * 16)
-            print("Average Loss: {}".format(avg_loss))
-            print("noprog ", noprog)
-            if avg_loss >= best_epoch[1]:
-                scheduler.step()
-                noprog +=1
-                if noprog >= 3:
-                    break
-            if avg_loss < best_epoch[1]:
-                best_epoch = (epoch_count, avg_loss)
-                noprog = 0
+        # if i == len(train_dataloader) - 1:
+        #     avg_loss = running_loss / (i * 16)
+        #     print("Average Loss: {}".format(avg_loss))
+        #     print("noprog ", noprog)
+        #     if avg_loss >= best_epoch[1]:
+        #         scheduler.step()
+        #         noprog +=1
+        #         if noprog >= 3:
+        #             break
+        #     if avg_loss < best_epoch[1]:
+        #         best_epoch = (epoch_count, avg_loss)
+        #         noprog = 0
+    epoch_loss = running_loss / (len(train_dataloader) * 16)
+    if best_epoch["loss"] >= epoch_loss:
+        print("Average Loss: {}".format(epoch_loss))
+        best_epoch = {"epoch_num":epoch_count, "loss":epoch_loss}
+        no_prog = 0
+    else:
+        no_prog += 1
+        if no_prog == 3:
+            scheduler.step()
     epoch_count += 1
-print(best_epoch)
 
 
 
